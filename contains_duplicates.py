9b8ee32af7b7e9e1c034e54c880066b349768a2a
Justin Trombley <jtrombley96@gmail.com>
2020-07-29 22:21:39 -0400

    Two Sum

--- contains_duplicates.py
+++ contains_duplicates.py
@@ -1,5 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        
+
         # 13/18 Tests Passed
 
         # if len(nums) >= 2:
@@ -13,3 +16,14 @@ class Solution:
         # Memory: 19.2 MB
         if len(set(nums)) < len(nums):
             return True
+
+# def duplicates(nums):
+#     numbers = set()
+
+#     for num in nums:
+#         if num in numbers:
+#             return True
+#         numbers.add(num)
+#     print(numbers)
+
+# print(duplicates([5, 6, 7, 8]))
